b7aed11fffe94dc41f525e6e9d2e7060411d0cda
Oleh Krupenia <oleh.krupenia@globallogic.com>
2018-02-12 18:49:45 +0200

    Migration to Java 9

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.testdev.reactive.main {
+    requires myJar;
+    requires spring.beans;
+    requires spring.web;
+    requires reactor.core;
+    requires mongodb.driver.reactivestreams;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.data.mongodb;
+    requires spring.data.commons;
+}
